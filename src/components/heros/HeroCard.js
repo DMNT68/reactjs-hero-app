19a7b164c95dbfd8a6630397d15cf672e06d4a6d
Andrés Salgado <andressalgado41@gmail.com>
2021-07-02 23:30:27 -0500

    mejor manejo de imgs

--- src/components/heros/HeroCard.js
+++ src/components/heros/HeroCard.js
@@ -1,27 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+const heroImages = require.context('../../assets/heroes', true);
 
-export const HeroCard = ({ id, superhero, alter_ego, first_appearance, characters, }) => {
-    return (
-        <div className="card ms-3 mb-3" style={{maxWidth: 300,}}>
-            <div className="d-flex justify-content-md-around flex-wrap">
-                <div style={{width:'40%'}}>
-                    <img src={`./assets/heroes/${id}.jpg`} className="card-img" alt={superhero} />
-                </div>
-                <div style={{width:'60%'}}>
-                    <div className="card-body">
-                        <h5 className="card-title">{superhero}</h5>
-                        <p className="card-text">{alter_ego}</p>
-                        {
-                            (alter_ego !== characters) && <p className="card-text">{characters}</p>
-                        }
-                        <p className="card-text">
-                            <small className="text-muted">{first_appearance}</small>
-                        </p>
-                        <Link to={`./hero/${id}`}>Mas...</Link>
-                    </div>
-                </div>
-            </div>
+export const HeroCard = ({ id, superhero, alter_ego, first_appearance, characters }) => {
+  return (
+    <div className="card ms-3 mb-3" style={{ maxWidth: 300 }}>
+      <div className="d-flex justify-content-md-around flex-wrap">
+        <div style={{ width: '40%' }}>
+          <img src={heroImages(`./${id}.jpg`).default} className="card-img" alt={superhero} />
         </div>
-    )
-}
+        <div style={{ width: '60%' }}>
+          <div className="card-body">
+            <h5 className="card-title">{superhero}</h5>
+            <p className="card-text">{alter_ego}</p>
+            {alter_ego !== characters && <p className="card-text">{characters}</p>}
+            <p className="card-text">
+              <small className="text-muted">{first_appearance}</small>
+            </p>
+            <Link to={`./hero/${id}`}>Mas...</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/heros/HeroScreen.js
+++ src/components/heros/HeroScreen.js
@@ -1,6 +1,9 @@
 import React, { useMemo } from 'react'
 import { Redirect, useParams } from 'react-router-dom'
+import { heroImages } from '../../helpers/heroImages';
 import {getHeroById} from '../../selectors/getHeroById';
+
+
 
 export const HeroScreen = ({history}) => {
 
@@ -24,7 +27,7 @@ export const HeroScreen = ({history}) => {
     return (
         <div className="row mt-5">
             <div className="col-4">
-                <img src={`../assets/heroes/${heroeId}.jpg`} className="img-thumbnail animate__animated animate__fadeInLeft" alt={superhero} />
+                <img src={heroImages(`./${heroeId}.jpg`).default} className="img-thumbnail animate__animated animate__fadeInLeft" alt={superhero} />
             </div>
             <div className="col-8 animate__animated animate__fadeIn">
                 <h3>{superhero}</h3>
